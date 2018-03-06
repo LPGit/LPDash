4079be35c7aebfcf5f4cfaff7d78135f1ac7c71d
Nicholas Baumgaertner <n.baumgaertner@gmail.com>
2018-03-06 06:01:53 +0100

    added react

--- src/test.js
+++ src/test.js
@@ -1,4 +1,33 @@
+ const React = require('react');
+ const ReactDOM = require('react-dom');
+
+
+ class HelloMessage extends React.Component {
+    render() {
+      return React.createElement(
+        "div",
+        { class: 'hello-message' },
+        "Hello ",
+        this.props.name
+      );
+    }
+  }
+  
+  ReactDOM.render(React.createElement(HelloMessage, { name: "Taylor" }), document.getElementById('root'));
+
+
+
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('root')
+// );
+
+
+
+
 document.getElementById('test-button').addEventListener('click', function () {
     console.log("terror");
     document.getElementById("test-button").innerHTML = "YOU CLICKED ME!";
 });
+
+
